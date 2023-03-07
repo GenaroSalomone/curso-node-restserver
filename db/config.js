2990915e9b7faa2cb5b302d1d4ccfcd68dcea684
MisterGena <genarosalomone@gmail.com>
2023-03-07 16:59:04 -0300

    testing

--- db/config.js
+++ db/config.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
     try {
+        mongoose.set('strictQuery', true);
 
         await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
