52454d8d3a2ff7cc4c599fa676bfd1cb6fc2a08b
Genaro Salomone <genarosalomone@gmail.com>
2023-11-17 11:39:06 -0300

    Up to class 179

---
+++ routes/categorias.js
@@ -0,0 +1,36 @@
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarJWT, validarCampos } = require('../middlewares');
+const { crearCategoria } = require('../controllers/categorias');
+
+const router = Router();
+
+// Obtener todas las categorias -> publico
+router.get('/', (req, res) => {
+  res.json('get')
+})
+
+// Obtener una categoria por id -> publico
+router.get('/:id', (req, res) => {
+  res.json('get - id')
+})
+
+// Crear categoria -> privado (cualquier rol)
+router.post('/', [
+  validarJWT,
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  validarCampos
+], crearCategoria )
+
+// Actualizar -> privado (cualquier rol)
+router.put('/:id', (req, res) => {
+  res.json('put')
+})
+
+// Borrar categoria -> admin
+router.delete('/:id', (req, res) => {
+  res.json('delete')
+})
+module.exports = router;
