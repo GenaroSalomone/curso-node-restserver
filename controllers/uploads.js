e5fc5d37d78212fa7650f76b574f0944eacf3fd0
Genaro Salomone <genarosalomone@gmail.com>
2024-01-24 18:06:38 -0300

    Ruta para actualizar imagenes de usuarios y productos con middleware personalizado

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,22 +1,45 @@
-
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
+const { Usuario, Producto } = require("../models");
 
 const cargarArchivo = async (req, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-    res.status(400).json({ msg: "No hay archivos en la petición" });
-    return;
-  }
-
   try {
-    const nombre = await subirArchivo(req.files, undefined, 'imgs');
-    res.json({ nombre })
-
+    const nombre = await subirArchivo(req.files, undefined, "imgs");
+    res.json({ nombre });
   } catch (error) {
-    res.status(400).json({ msg: error })
+    res.status(400).json({ msg: error });
   }
+};
+
+const actualizarImagen = async (req, res = response) => {
+  const { id, coleccion } = req.params;
+
+  let modelo;
+  switch (coleccion) {
+    case "usuarios":
+      modelo = await Usuario.findById(id);
+      if (!modelo) {
+        return res.status(400).json({ msg: `No existe usuario con el ID: ${id}` });
+      }
+      break;
+
+    case "productos":
+      modelo = await Producto.findById(id);
+      if (!modelo) {
+        return res.status(400).json({ msg: `No existe producto con el ID: ${id}` });
+      }
+      break;
+
+    default:
+      return res.status(500).json({ msg: "Se me olvidó validar esto" });
+  }
+  const nombre = await subirArchivo(req.files, undefined, coleccion);
+  modelo.img = nombre;
+  await modelo.save();
+  res.json(modelo);
 };
 
 module.exports = {
   cargarArchivo,
+  actualizarImagen,
 };
--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,49 +1,49 @@
 
 const Role = require('../models/role');
-const {Usuario, Categoria, Producto} = require('../models');
+const { Usuario, Categoria, Producto } = require('../models');
 const mongoose = require('mongoose');
 
-const esRoleValido = async ( rol = '' ) => {
-    const existeRol = await Role.findOne ( {rol} );
-    if ( !existeRol ) {
-        throw new Error (`El rol ${ rol } no está registrado en la BD`)
+const esRoleValido = async (rol = '') => {
+    const existeRol = await Role.findOne({ rol });
+    if (!existeRol) {
+        throw new Error(`El rol ${rol} no está registrado en la BD`)
     }
 }
 
-const emailExiste = async ( correo = '') => {
+const emailExiste = async (correo = '') => {
 
-    const existeEmail = await Usuario.findOne( { correo } );
-    if ( existeEmail ) {
-        throw new Error (`El correo ${ correo } ya está registrado en la BD`);
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        throw new Error(`El correo ${correo} ya está registrado en la BD`);
     }
 }
 
-const existeUsuarioPorId = async ( id ) => {
-    
-    if ( !mongoose.Types.ObjectId.isValid( id ) ) {
+const existeUsuarioPorId = async (id) => {
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new Error(`No es un ID de Mongoose valido`);
     }
-    
+
     const existeUsuario = await Usuario.findById(id);
- 
-    if ( !existeUsuario ) {
-        throw new Error(`El usuario con el ID '${ id }' no existe`);
+
+    if (!existeUsuario) {
+        throw new Error(`El usuario con el ID '${id}' no existe`);
     }
 };
 
-const existeCategoriaPorId = async ( id ) => {
+const existeCategoriaPorId = async (id) => {
     const categoria = await Categoria.findById(id);
-  
-    if ( !categoria ) {
-        throw new Error(`La categoria con el ID '${ id }' no existe`);
+
+    if (!categoria) {
+        throw new Error(`La categoria con el ID '${id}' no existe`);
     }
 }
 
-const existeProductoPorId = async ( id ) => {
+const existeProductoPorId = async (id) => {
     const producto = await Producto.findById(id);
-  
-    if ( !producto ) {
-        throw new Error(`El producto con el ID '${ id }' no existe`);
+
+    if (!producto) {
+        throw new Error(`El producto con el ID '${id}' no existe`);
     }
 }
 
@@ -51,9 +51,9 @@ const existeCategoriaPorNombre = async (nombre) => {
     nombre = nombre.toUpperCase();
     const categoriaDB = await Categoria.findOne({ nombre });
     if (categoriaDB) {
-      throw new Error(`La categoria ${nombre}, ya existe`);
+        throw new Error(`La categoria ${nombre}, ya existe`);
     }
-  }
+}
 
 const existeProductoPorNombre = async (nombre) => {
     nombre = nombre.toUpperCase();
@@ -64,6 +64,12 @@ const existeProductoPorNombre = async (nombre) => {
     }
 };
 
+const coleccionesPermitidas = (coleccion = '', colecciones = []) => {
+    const incluida = colecciones.includes(coleccion);
+    if (!incluida) throw new Error(`La coleccion ${coleccion} no esta permitida`);
+    return true;
+}
+
 module.exports = {
     esRoleValido,
     emailExiste,
@@ -71,7 +77,8 @@ module.exports = {
     existeCategoriaPorId,
     existeCategoriaPorNombre,
     existeProductoPorNombre,
-    existeProductoPorId
+    existeProductoPorId,
+    coleccionesPermitidas
 }
 
 
